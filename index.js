5e90f33ac72001747402b1df8c71c24657d0e507
Asa Holland <asaleholland@gmail.com>
2021-10-18 22:56:30 -0700

    update index

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const cors = require('cors')
 
 // Create the server
 const app = express()
+
+const path = require('path');
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 // Serve our api route that returns a custom text
-app.get('/api/:say', cors(), async (req, res, next) => {
+app.get('/api/track/:id', cors(), async (req, res, next) => {
   try {
-    const text = req.params.say
+    const text = req.params.id
     const test_text = { text: 'Hello World!' }
     res.json({ test_text, text })
   } catch (err) {
@@ -15,7 +26,7 @@ app.get('/api/:say', cors(), async (req, res, next) => {
 })
 
 // Serve our base route that returns a Hello World text
-app.get('/api/', cors(), async (req, res, next) => {
+app.get('/api/track/', cors(), async (req, res, next) => {
   try {
     const test_text = { text: 'Hello World!' }
     res.json({ test_text })
@@ -27,5 +38,5 @@ app.get('/api/', cors(), async (req, res, next) => {
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`Mixing it up on port http://localhost:${PORT}/api/id`)
+  console.log(`Mixing it up on port http://localhost:${PORT}/api/track/`)
 })
