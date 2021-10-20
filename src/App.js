9144de54268b2b9da49c63416292a0d01d36f374
Asa Holland <asaleholland@gmail.com>
2021-10-19 21:22:38 -0700

    create basic header bar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>
-          Search for a song, we'll do the rest!
-        </h1>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Welcome to <code>Tunescout</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar />
+        <div className="App">
+              <header className="App-header">
+              <h1>
+                  Search for a song, we'll do the rest!
+                </h1>
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                <p>
+                  Welcome to <code>Tunescout</code>.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+    </Router>
+   
   );
 }
 
