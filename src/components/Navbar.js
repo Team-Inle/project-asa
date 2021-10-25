537187f6f20eec478cd8ee5752a201957fd57f05
Asa Holland <asaleholland@gmail.com>
2021-10-25 12:35:53 -0700

    remove links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,48 +44,8 @@ function Navbar() {
             <div className='menu-icon' onClick={handleClick}>
               {click ? <FaTimes /> : <FaBars />}
             </div>
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-              <li className='nav-item'>
-                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                  Home
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link
-                  to='/services'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  Services
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link
-                  to='/products'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  Products
-                </Link>
-              </li>
-              <li className='nav-btn'>
-                {button ? (
-                  <Link to='/sign-up' className='btn-link'>
-                    {/* <Button buttonStyle='btn--outline'>SIGN UP</Button> */}
-                  </Link>
-                ) : (
-                  <Link to='/sign-up' className='btn-link'>
-                    {/* <Button
-                      buttonStyle='btn--outline'
-                      buttonSize='btn--mobile'
-                      onClick={closeMobileMenu}
-                    >
-                      SIGN UP
-                    </Button> */}
-                  </Link>
-                )}
-              </li>
-            </ul>
+
+           
           </div>
         </nav>
       </IconContext.Provider>
